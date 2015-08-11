515650933dcbfde54ea7fe339bbe855c26d51c67
cyricx <contact@chrisbyram.com>
2015-08-10 23:02:29 -0500

    Setup basic test

---
+++ AngularAndRequireJS/Scripts/Application/MainApp.Bootstrap.js
@@ -0,0 +1,9 @@
+﻿//bootstrap is a term meant to imply putting your boots on and has no relation to the bootstrap library
+
+//all files written to be used for require follow a pattern of
+//define([array of files and dependencies], function (parameters from files) { stuff });
+define([
+    'testing'
+], function () {
+    
+});
---
+++ AngularAndRequireJS/Scripts/Application/MainApp.Require.js
@@ -0,0 +1,16 @@
+﻿//you will have access to the require object, which you can call it's configuration method
+require.config({
+	/*it has several properties of use
+		paths: this is for "aliasing" file paths
+			all paths are relative to this file
+	*/
+	paths: {
+		'testing':'../Test' //do not add .js to the end, require will do that for you
+	},
+
+
+	//deps: is used to declare a file that must be loaded before all else. This is typically
+	//used to hook up angular
+	deps: ['./MainApp.Bootstrap']
+
+})
